235ece419b1a3e4280b67fd5e0dca4ed6f6e166b
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-05-30 22:27:43 +0300

    Update the Attribute of the  all models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -4,7 +4,7 @@ import uuid
 
 # messaging_app/messaging_app/chats/models.py location of mymodels 
 class User(AbstractUser):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # primary_key & uuid
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # primary_key & uuid
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=150)
@@ -19,13 +19,18 @@ class User(AbstractUser):
 
 #define the Conversation models for the chats app  
 class Conversation(models.Model):
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # primary_key & uuid
     participants = models.ManyToManyField(User, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 # this is the Message Model to store messages in the chats app
 class Message(models.Model):
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # unique identifier
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
-    text = models.TextField()
-    timestamp = models.DateTimeField(auto_now_add=True)
+    message_body = models.TextField()  # replaces 'text'
+    sent_at = models.DateTimeField(auto_now_add=True)  # replaces 'timestamp'
+
+    def __str__(self):
+        return f"From {self.sender.email} at {self.sent_at}"
