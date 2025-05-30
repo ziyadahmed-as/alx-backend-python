4b8bb70f752ae112cb1029c9044a19b03bee534a
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-05-30 22:23:07 +0300

    add User model details

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+import uuid
 
 # messaging_app/messaging_app/chats/models.py location of mymodels 
 class User(AbstractUser):
-    pass  # Extend with additional fields if needed
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)  # primary_key & uuid
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return self.email
 
 #define the Conversation models for the chats app  
 class Conversation(models.Model):
