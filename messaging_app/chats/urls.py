9340308232c19cc7502d23cc4b68fc7694da4b27
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-05-30 22:40:09 +0300

    routing method

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -1,14 +1,16 @@
-# this used to define the URL patterns for the chats app in a Django project.
+# This file defines the URL patterns for the chats app in a Django project.
 # messaging_app/messaging_app/chats/urls.py
+
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter  #  routers.DefaultRouter() is used here
 from .views import ConversationViewSet, MessageViewSet
 
+# Initialize DefaultRouter to automatically generate URL routes for viewsets
 router = DefaultRouter()
-router.register(r'conversations', ConversationViewSet)
-router.register(r'messages', MessageViewSet)
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
 
-# The urlpatterns list routes URLs to views. For more information please see:
+# Include router-generated URLs in the urlpatterns
 urlpatterns = [
     path('', include(router.urls)),
 ]
