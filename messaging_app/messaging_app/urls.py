cc4d34d13f98cdded39cb4aed6b38af8a1214c57
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-05-30 22:47:26 +0300

    I add Authentication to Routing

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('chats.urls')),
+    
+    path('api-auth/', include('rest_framework.urls')), # Enables the browsable API and authentication
 ]
