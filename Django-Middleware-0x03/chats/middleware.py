bcf589b0ee4d2feb8c6e317ea10d7d5c14017b9c
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-07 17:13:08 +0300

    commited for midle ware file under chat

---
+++ Django-Middleware-0x03/chats/middleware.py
@@ -0,0 +1,23 @@
+# chats/middleware.py
+import logging
+from datetime import datetime
+
+# Configure logger to write to requests.log
+logger = logging.getLogger("request_logger")
+handler = logging.FileHandler("requests.log")
+formatter = logging.Formatter('%(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
+
+class RequestLoggingMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        user = request.user if request.user.is_authenticated else "Anonymous"
+        log_message = f"{datetime.now()} - User: {user} - Path: {request.path}"
+        logger.info(log_message)
+
+        response = self.get_response(request)
+        return response
--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -86,6 +86,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'chats.middleware.RequestLoggingMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
