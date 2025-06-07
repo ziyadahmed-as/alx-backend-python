30c8b455b3eab684f9aad4675c27b7a3a6e3ceb7
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-07 14:59:32 +0300

    permision class

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,9 +1,10 @@
 # I want to create the permissions for the chats app that allows users to view and send messages in a chat only if they are a member of that chat.
-# # messaging_app/chats/permissions.py
+# messaging_app/chats/permissions.py
+
+from rest_framework import permissions
 from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsParticipantOfConversation(BasePermission):
-   
     """
     Custom permission to allow only participants of a conversation
     to access the messages.
@@ -12,7 +13,6 @@ class IsParticipantOfConversation(BasePermission):
     def has_permission(self, request, view):
         # Allow safe methods (GET, HEAD, OPTIONS) for all users
         return request.user and request.user.is_authenticated
-
 
     def has_object_permission(self, request, view, obj):
         # Check if the user is a participant in the conversation
