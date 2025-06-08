fe1d5dcd5fd23764943050894aeb77e2d29618aa
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-08 10:04:56 +0300

    I add  role based midleware in my setting

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -86,7 +86,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    
+
+    'chats.middleware.RolepermissionMiddleware',
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
