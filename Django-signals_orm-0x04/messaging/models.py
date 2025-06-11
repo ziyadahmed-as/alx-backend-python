9c1ffe562b2e1be2dd583d014884f112ff2a73b9
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-11 07:12:06 +0300

    I add messaging edit models and signal log file adedd including

--- Django-signals_orm-0x04/messaging/models.py
+++ Django-signals_orm-0x04/messaging/models.py
@@ -3,14 +3,24 @@ from django.contrib.auth.models import User
 
 # Create a Message model with fields like sender, receiver, content, and timestamp.
 class Message(models.Model):
-    sender = models.ForeignKey('User', related_name='sent_messages', on_delete=models.CASCADE)
-    receiver = models.ForeignKey('User', related_name='received_messages', on_delete=models.CASCADE)
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
+    edited = models.BooleanField(default=False)
+    edited_at = models.DateTimeField(null=True, blank=True)
+    edited_by = models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL, related_name='edited_messages')
 
     def __str__(self):
-        return f"Message from {self.sender.username} to {self.receiver.username} at {self.timestamp}"
-    
+        return f"From {self.sender} to {self.receiver}"
+
+class MessageHistory(models.Model):
+    message = models.ForeignKey(Message, related_name='history', on_delete=models.CASCADE)
+    old_content = models.TextField()
+    changed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"History of Message ID {self.message.id}"
 # Create a Notification model to store notifications related to messages.
 
 class Notification(models.Model):
--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,8 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import pre_save
 from django.dispatch import receiver
-from .models import Message, Notification
+from django.utils import timezone
+from .models import Message, MessageHistory, Notification
+from django.db.models.signals import post_save
 # Define a custom signal for message creation
 
 @receiver(post_save, sender=Message)
@@ -13,3 +15,15 @@ def create_notification(sender, instance, created, **kwargs):
             user=instance.receiver,
             message=instance
         )
+
+@receiver(pre_save, sender=Message)
+def log_message_edit(sender, instance, **kwargs):
+    if instance.pk:  # Only if this is an update
+        old_message = Message.objects.get(pk=instance.pk)
+        if old_message.content != instance.content:
+            instance.edited = True
+            instance.edited_at = timezone.now()
+            MessageHistory.objects.create(
+                message=old_message,
+                old_content=old_message.content
+            )
