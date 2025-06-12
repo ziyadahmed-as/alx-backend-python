33df8cef262a25cb6626e798844417e6b26cf13b
Ziyad Ahmed <ziyafwru.12@gmail.com>
2025-06-12 06:48:27 +0300

    Django signals to delete user-related data when a user deletes their account.

--- Django-signals_orm-0x04/django_chat/Models/signals.py
+++ Django-signals_orm-0x04/django_chat/Models/signals.py
@@ -1,5 +1,10 @@
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import Message, MessageHistory, Notification
+from .notification import Notification
 from .message import Message
 from .message_history import MessageHistory
 
@@ -16,3 +21,16 @@ def log_message_edit(sender, instance, **kwargs):
                 instance.edited = True
         except Message.DoesNotExist:
             pass
+
+
+@receiver(post_delete, sender=User)
+def delete_user_related_data(sender, instance, **kwargs):
+    # Delete messages sent or received by user
+    Message.objects.filter(sender=instance).delete()
+    Message.objects.filter(receiver=instance).delete()
+
+    # Delete related message histories
+    MessageHistory.objects.filter(message__sender=instance).delete()
+
+    # Delete notifications related to the user
+    Notification.objects.filter(user=instance).delete()
--- Django-signals_orm-0x04/django_chat/views.py
+++ Django-signals_orm-0x04/django_chat/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+
+@login_required
+def delete_user(request):
+    user = request.user
+    logout(request)  # Log out the user before deletion
+    user.delete()    # Triggers post_delete signal
+    return redirect('home')  # Replace 'home' with your homepage URL name
